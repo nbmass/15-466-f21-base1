4259fd11de6c1d52f78d1163943f17e76e5c9238
Nolan Mass <nolanbmass@gmail.com>
2021-09-14 01:20:52 -0400

    pretty much done

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -1,4 +1,5 @@
 #include "PlayMode.hpp"
+#include "load_save_png.hpp"
 
 //for the GL_ERRORS() macro:
 #include "gl_errors.hpp"
@@ -18,90 +19,93 @@ PlayMode::PlayMode() {
 
 	//Also, *don't* use these tiles in your game:
 
-	{ //use tiles 0-16 as some weird dot pattern thing:
-		std::array< uint8_t, 8*8 > distance;
-		for (uint32_t y = 0; y < 8; ++y) {
-			for (uint32_t x = 0; x < 8; ++x) {
-				float d = glm::length(glm::vec2((x + 0.5f) - 4.0f, (y + 0.5f) - 4.0f));
-				d /= glm::length(glm::vec2(4.0f, 4.0f));
-				distance[x+8*y] = std::max(0,std::min(255,int32_t( 255.0f * d )));
-			}
+
+	//load images
+	auto load_image = [&](std::string file_name, int tile_index, int pallette_index) {
+		// std::string file_name = "dist/test.png";
+		glm::uvec2 size = glm::uvec2(8, 8);
+		std::vector<glm::u8vec4> data;
+		OriginLocation origin = OriginLocation::LowerLeftOrigin;
+		load_png(file_name, &size, &data, origin);
+
+		//set pallette
+		for (int color = 0; color < 4; color++) {
+			ppu.palette_table[pallette_index][color] = glm::uvec4(0, 0, 0, 0);
 		}
-		for (uint32_t index = 0; index < 16; ++index) {
-			PPU466::Tile tile;
-			uint8_t t = (255 * index) / 16;
-			for (uint32_t y = 0; y < 8; ++y) {
-				uint8_t bit0 = 0;
-				uint8_t bit1 = 0;
-				for (uint32_t x = 0; x < 8; ++x) {
-					uint8_t d = distance[x+8*y];
-					if (d > t) {
-						bit0 |= (1 << x);
-					} else {
-						bit1 |= (1 << x);
+		int current_color = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				bool found = false;
+				for (int color = 0; color < 3; color++) {
+					if (data[8*i+j] == ppu.palette_table[pallette_index][color]) {
+						found = true;
 					}
 				}
-				tile.bit0[y] = bit0;
-				tile.bit1[y] = bit1;
+				if (!found and current_color < 3) {
+					ppu.palette_table[pallette_index][current_color] = data[8*i+j];
+					current_color++;
+				}
 			}
-			ppu.tile_table[index] = tile;
+		}
+		
+		//set tile
+		for (int i = 0; i < 8; i++) {
+			ppu.tile_table[tile_index].bit0[i] = 0;
+			ppu.tile_table[tile_index].bit1[i] = 0;
+			for (int j = 0; j < 8; j++) {
+				for (int color = 0; color < 3; color++) {
+					if (data[8*i+j] == ppu.palette_table[pallette_index][color]) {
+						ppu.tile_table[tile_index].bit0[i] |= (color % 2) << j;
+						ppu.tile_table[tile_index].bit1[i] |= (color / 2) << j;
+					}
+				}
+			}
+		}
+	};
+
+	//load player
+	load_image("dist/player_up.png", 32, 7);
+	load_image("dist/player_down.png", 33, 7);
+	load_image("dist/player_left.png", 34, 7);
+	load_image("dist/player_right.png", 35, 7);
+	load_image("dist/player_upleft.png", 36, 7);
+	load_image("dist/player_upright.png", 37, 7);
+	load_image("dist/player_downleft.png", 38, 7);
+	load_image("dist/player_downright.png", 39, 7);
+
+	//load enemies
+	load_image("dist/enemy.png", 31, 6);
+
+	//load projectile images
+	load_image("dist/laser11.png", 16, 2);
+	load_image("dist/laser1-1.png", 17, 2);
+	load_image("dist/laser01.png", 18, 2);
+	load_image("dist/laser10.png", 19, 2);
+
+	//background tiles
+	load_image("dist/background_empty.png", 0, 0);
+	load_image("dist/background_smallstar.png", 1, 0);
+	load_image("dist/background_bigstar.png", 2, 0);
+	load_image("dist/background_moon.png", 4, 0);
+	load_image("dist/background_xstar.png", 3, 0);
+
+	for (uint32_t y = 0; y < PPU466::BackgroundHeight; ++y) {
+		for (uint32_t x = 0; x < PPU466::BackgroundWidth; ++x) {
+			if ((std::rand() % 8) == 0) {
+				ppu.background[x+PPU466::BackgroundWidth*y] = (std::rand() % 4);
+			} else {
+				ppu.background[x+PPU466::BackgroundWidth*y] = 0;
+			}
 		}
 	}
+	//make moon
+	ppu.background[PPU466::BackgroundWidth * 19 + 24] = 4;
 
-	//use sprite 32 as a "player":
-	ppu.tile_table[32].bit0 = {
-		0b01111110,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b01111110,
-	};
-	ppu.tile_table[32].bit1 = {
-		0b00000000,
-		0b00000000,
-		0b00011000,
-		0b00100100,
-		0b00000000,
-		0b00100100,
-		0b00000000,
-		0b00000000,
-	};
-
-	//makes the outside of tiles 0-16 solid:
-	ppu.palette_table[0] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
-
-	//makes the center of tiles 0-16 solid:
-	ppu.palette_table[1] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
-
-	//used for the player:
-	ppu.palette_table[7] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0xff, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-	};
-
-	//used for the misc other sprites:
-	ppu.palette_table[6] = {
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x88, 0x88, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	};
-
+	for (int i = 1; i < 32; i++) {
+			enemies[i].pos.x = std::rand() % 256;
+			enemies[i].pos.y = std::rand() % 240;
+			enemies[i].dir = directions[std::rand() % 8];
+	}
 }
 
 PlayMode::~PlayMode() {
@@ -126,6 +130,10 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			down.downs += 1;
 			down.pressed = true;
 			return true;
+		} else if (evt.key.keysym.sym == SDLK_SPACE) {
+			space.downs += 1;
+			space.pressed = true;
+			return true;
 		}
 	} else if (evt.type == SDL_KEYUP) {
 		if (evt.key.keysym.sym == SDLK_LEFT) {
@@ -136,9 +144,11 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			return true;
 		} else if (evt.key.keysym.sym == SDLK_UP) {
 			up.pressed = false;
-			return true;
 		} else if (evt.key.keysym.sym == SDLK_DOWN) {
 			down.pressed = false;
+			return true;
+		} else if (evt.key.keysym.sym == SDLK_SPACE) {
+			space.pressed = false;
 			return true;
 		}
 	}
@@ -159,51 +169,140 @@ void PlayMode::update(float elapsed) {
 	if (down.pressed) player_at.y -= PlayerSpeed * elapsed;
 	if (up.pressed) player_at.y += PlayerSpeed * elapsed;
 
+	if (player_at.x < 0) player_at.x += 256;
+	if (player_at.x > 256) player_at.x -= 256;
+	if (player_at.y < 0) player_at.y += 256;
+	if (player_at.y > 256) player_at.y -= 256;
+
+	//player direction
+	if (left.pressed or right.pressed or down.pressed or up.pressed) {
+		player_direction = glm::vec2(0, 0);
+		if (left.pressed) player_direction.x = -1;
+		if (right.pressed) player_direction.x = 1;
+		if (down.pressed) player_direction.y = -1;
+		if (up.pressed) player_direction.y = 1;
+	}
+
+	for (int i = 1; i < 32; i++) {
+		Character &e = enemies[i];
+		if (e.is_alive) {
+			e.pos += elapsed * PlayerSpeed * e.dir;
+			if (e.pos.x < 0) e.pos.x += 256;
+			if (e.pos.x > 256) e.pos.x -= 256;
+			if (e.pos.y < 0) e.pos.y += 256;
+			if (e.pos.y > 256) e.pos.y -= 256;
+			//change direction
+			int d = std::rand() % 1000;
+			if (d < 8) {
+				e.dir = directions[d];
+			}
+			if (!e.fired and (projectiles.size() < 32) and (std::rand() % 2000 < score)) {
+				projectiles.push_back(Projectile(e.pos.x, e.pos.y, e.dir.x, e.dir.y));
+				projectiles.back().ignore_enemies = true;
+			}
+		}
+	}
+
+	//update and delete old projectiles
+	constexpr float ProjectileSpeed = 100.0f;
+	for (auto &p : projectiles) {
+		if (p.pos.x < 0 or p.pos.x >= 255 or p.pos.y < 0 or p.pos.y >= 239) {
+			projectiles.pop_front();
+		} else {
+			p.pos += ProjectileSpeed * p.dir * elapsed;
+		}
+	}
+	//shoot
+	if (space.downs > 0 and projectiles.size() < 32) {
+		space.downs -= 1;
+		projectiles.push_back(Projectile(player_at.x, player_at.y, player_direction.x, player_direction.y));
+	}
+	//check collisions
+	for (auto &p : projectiles) {
+		if (p.ignore_enemies) {
+			if (glm::length(player_at - p.pos) <= 4.0f) {
+				score = 0;
+				//kill player
+				player_at.y = 240;
+				//kill projectile
+				projectiles.pop_front();
+				break;
+			}
+		} else {
+			for (uint32_t i = 1; i < 32; i++) {
+				if (!enemies[i].is_alive) continue;
+				glm::vec2 dist = enemies[i].pos - p.pos;
+				if (glm::length(dist) <= 4.0f) {
+					score += 1;
+					//kill enemy
+					enemies[i].pos.y = 240;
+					enemies[i].is_alive = false;
+					//kill projectile
+					projectiles.pop_front();
+					break;
+				}
+			}
+		}
+	}
+
 	//reset button press counters:
 	left.downs = 0;
 	right.downs = 0;
 	up.downs = 0;
 	down.downs = 0;
+	space.downs = 0;
 }
 
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
 	//--- set ppu state based on game state ---
 
 	//background color will be some hsv-like fade:
-	ppu.background_color = glm::u8vec4(
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 1.0f / 3.0f) ) ) ))),
-		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 2.0f / 3.0f) ) ) ))),
-		0xff
-	);
-
-	//tilemap gets recomputed every frame as some weird plasma thing:
-	//NOTE: don't do this in your game! actually make a map or something :-)
-	for (uint32_t y = 0; y < PPU466::BackgroundHeight; ++y) {
-		for (uint32_t x = 0; x < PPU466::BackgroundWidth; ++x) {
-			//TODO: make weird plasma thing
-			ppu.background[x+PPU466::BackgroundWidth*y] = ((x+y)%16);
-		}
-	}
+	// ppu.background_color = glm::u8vec4(
+	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
+	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
+	// 	std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
+	// 	0xff
+	// );
 
 	//background scroll:
-	ppu.background_position.x = int32_t(-0.5f * player_at.x);
-	ppu.background_position.y = int32_t(-0.5f * player_at.y);
+	// ppu.background_position.x = int32_t(-0.5f * player_at.x);
+	// ppu.background_position.y = int32_t(-0.5f * player_at.y);
 
 	//player sprite:
 	ppu.sprites[0].x = int32_t(player_at.x);
 	ppu.sprites[0].y = int32_t(player_at.y);
-	ppu.sprites[0].index = 32;
+	if (player_direction == glm::vec2(0,1)) ppu.sprites[0].index = 32;
+	if (player_direction == glm::vec2(0,-1)) ppu.sprites[0].index = 33;
+	if (player_direction == glm::vec2(-1,0)) ppu.sprites[0].index = 34;
+	if (player_direction == glm::vec2(1,0)) ppu.sprites[0].index = 35;
+	if (player_direction == glm::vec2(-1,1)) ppu.sprites[0].index = 36;
+	if (player_direction == glm::vec2(1,1)) ppu.sprites[0].index = 37;
+	if (player_direction == glm::vec2(-1,-1)) ppu.sprites[0].index = 38;
+	if (player_direction == glm::vec2(1,-1)) ppu.sprites[0].index = 39;
 	ppu.sprites[0].attributes = 7;
 
 	//some other misc sprites:
-	for (uint32_t i = 1; i < 63; ++i) {
-		float amt = (i + 2.0f * background_fade) / 62.0f;
-		ppu.sprites[i].x = int32_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].y = int32_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
-		ppu.sprites[i].index = 32;
+	for (uint32_t i = 1; i < 32; ++i) {
+		ppu.sprites[i].x = enemies[i].pos.x;
+		ppu.sprites[i].y = enemies[i].pos.y;
+		ppu.sprites[i].index = 31;
 		ppu.sprites[i].attributes = 6;
-		if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
+	}
+
+	//projectiles:
+	for (uint32_t i = 0; i < 32; i++) {
+		if (i < projectiles.size()) {
+			Projectile &p = projectiles[i];
+			ppu.sprites[i+32].x = p.pos.x;
+			ppu.sprites[i+32].y = p.pos.y;
+			if (p.dir.x == p.dir.y) ppu.sprites[i+32].index = 16;
+			else if (p.dir.x == -p.dir.y) ppu.sprites[i+32].index = 17;
+			else if (p.dir.x == 0) ppu.sprites[i+32].index = 18;
+			else if (p.dir.y == 0) ppu.sprites[i+32].index = 19;
+			ppu.sprites[i+32].attributes = 2;
+		} else {
+			ppu.sprites[i+32].y = 255;
+		}
 	}
 
 	//--- actually draw ---
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -21,13 +21,49 @@ struct PlayMode : Mode {
 	struct Button {
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-	} left, right, down, up;
+	} left, right, down, up, space;
 
 	//some weird background animation:
 	float background_fade = 0.0f;
 
 	//player position:
 	glm::vec2 player_at = glm::vec2(0.0f);
+	glm::vec2 player_direction = glm::vec2(1, 1);
+
+	std::vector<glm::vec2> directions{
+		glm::vec2(0,1),
+		glm::vec2(1,0),
+		glm::vec2(0,-1),
+		glm::vec2(-1,0),
+		glm::vec2(-1,1),
+		glm::vec2(-1,-1),
+		glm::vec2(1,1),
+		glm::vec2(1,-1)
+	};
+
+	//enemies:
+	struct Character {
+		glm::vec2 pos;
+		glm::vec2 dir;
+		bool fired = false;
+		bool is_alive = true;
+	};
+	std::vector<Character> enemies = std::vector<Character>(32);
+
+	struct Projectile {
+		glm::vec2 pos;
+		glm::vec2 dir;
+		bool ignore_enemies = false;
+		Projectile(float x, float y, float dx, float dy) {
+			pos = glm::vec2(x, y);
+			dir = glm::vec2(dx, dy);
+		}
+	};
+
+	std::deque<Projectile> projectiles;
+
+	//score:
+	uint32_t score = 0;
 
 	//----- drawing handled by PPU466 -----
 
